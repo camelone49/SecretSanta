b76040b0c24cef02430193dba6281578023a9033
Hisham <118437933+camelone49@users.noreply.github.com>
2025-05-15 11:37:46 +0200

    Update wichteln.m

--- wichteln.m
+++ wichteln.m
@@ -1,18 +1,18 @@
 clear;
 
-Bewohner = [1 2001 "Stella" "stellapietzonka@gmail.com"; 
-            2 2002 "Valentin" "vallegruetze@gmail.com";
-            3 2003 "Caro" "carolin@erben.com";
-            4 2004 "Tim" "tim.kleefisch@yahoo.com";
-            5 2005 "Sewon" "sewon.kim@rwth-aachen.de";
-            6 2006 "Alaia" "trecualaia@gmail.com";
-            7 2007 "Simon" "simon.rudi@rwth-aachen.de";
-            8 2008 "Marlene" "marlene.stoeger@gmail.com";
-            9 2010 "Hisham" "smart.hisham@gmail.com"
-            10 2011 "René" "rene.hartmann1@gmx.net";
-            11 2012 "Lydia" "lydia.krauss@outlook.com";
-            12 2099 "Laura" "lauras8_01@yahoo.fr";
-            13 2098 "Annika" "wenhao.yang111@gmail.com"];
+Bewohner = [1 2001 "Stella" "stellas mail"; 
+            2 2002 "Valentin" "valles mail";
+            3 2003 "Caro" "caros mail";
+            4 2004 "Tim" "Fischs mail";
+            5 2005 "Sewon" "Sewons Mail";
+            6 2006 "Alaia" "Alaias Mail";
+            7 2007 "Simon" "Simons Mail";
+            8 2008 "Marlene" "Marlenes Mail";
+            9 2010 "Hisham" "Meine Mail"
+            10 2011 "René" "Renes Mail";
+            11 2012 "Lydia" "Lydias Mail";
+            12 2099 "Laura" "Lauras Mail";
+            13 2098 "Annika" "Annikas Mail"];
         
 Bewohnerindex = Bewohner(:,1);
 
